d5129ff0455b7d246c1402dfbf0d2a52725d00a7
luksk <lukskill@hotmail.com>
2021-07-17 21:59:27 -0300

    configure database, migrations and environment variables

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+require("dotenv").config({
+    path: process.env.NODE_ENV == "test" ? ".env.test" : ".env"
+  });
+  
+  module.exports = {
+    username: 'postgres',//'postgres', //
+    password: 'postgres',//'123456', //  
+    host: '127.0.0.1',//'127.0.0.1', //
+    database: 'developers',//'development',//
+    dialect: 'postgres',// "postgres",  || "sqlite",
+    storage: "./__tests__/database.sqlite",
+    operatorsAliases: 0,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      uderscoredAll: true
+    }
+  }
+  
+  
